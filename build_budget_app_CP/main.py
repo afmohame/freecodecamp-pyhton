b57659a3881da5e5fecb8be0be0da3c4185a3ff6
Vajinbuuu <afakkir.mohamed2@gmail.com>
2026-02-06 21:03:02 +0100

    took some time but it is finished. Will work on making it better over time because not satisfied

--- build_budget_app_CP/main.py
+++ build_budget_app_CP/main.py
@@ -19,7 +19,7 @@ class Category:
         return True
     
     def get_balance(self):
-        return sum(i["amount"] for i in self.ledger)
+        return round(sum(i["amount"] for i in self.ledger), 2)
     
     def transfer(self, amount, new_category):
         if not self.check_funds(amount):
@@ -42,52 +42,65 @@ class Category:
             return True
 
     def __str__(self):
-        template = ""
         symbols_number = (self.max_chars - len(self.name))
-        insert_title = symbols_number//2 # // will throw the decimal away 
-        while symbols_number > 0:
-            if symbols_number == insert_title:
-                template += self.name
-            template += self.symbol
-            symbols_number -= 1
+        left = symbols_number//2 # // will throw the decimal away 
+        right = symbols_number - left
+        template = f"{'*'*left}{self.name}{'*'*right}"
         for i in self.ledger:
             descr = i['description']
             amount = f"{i['amount']:.2f}"
-            length_dict = len(str(amount)) + len(descr)
-            space_between = self.max_chars - length_dict
-            if length_dict > self.max_chars:
-                splice_descr = length_dict - self.max_chars + 1
-                #added the +1 so it matches the example
-                descr = descr[:-splice_descr]
-                length_dict = len(str(amount)) + len(descr)
-                space_between = self.max_chars - length_dict
-            template += f"\n{descr}{' '*space_between}{amount}"
+            padding = 30 - len(descr[:23]) - len(str(amount))
+            template += f"\n{descr[:23]}{' '*padding}{amount}"
         return f"{template}\nTotal: {self.get_balance()}"
 
+
+
 def create_spend_chart(categories):
-    spent = -round(sum(i['amount'] for i in categories.ledger[1:]), -1)
-    print(spent)
-    template = 'Percentage spent by category'
-    symbol = 'o'
+    bar_chart = "Percentage spent by category"
+    category_names = list(n.name for n in categories) #the names of the categories
+    max_len = max(len(l) for l in category_names)
+    spending_by_cate = {}
+    for i in categories:
+        spent = -(sum(n["amount"] for n in i.ledger if n['amount'] < 0)) #this sums up the withdrawals from one category at a time
+        spending_by_cate[i.name] = spent # this adds the category name and the spent value in the category 
+    spending_by_cate["Total"] = sum(spending_by_cate.values()) #total spending for all categories
+    percent_spend_per_cate = list(int((100*spending_by_cate[n]/spending_by_cate["Total"])/10)*10 for n in list(spending_by_cate)[:-1])
+    #This will fetch the spending per category and divide it by the total spending to have a percentage of total spending per category
     percent = 100
     y_axis = 4
     while percent >= 0:
-        space_y = y_axis - len(str(percent)+'|')
-        template += f"\n{' '*space_y}{percent}|"
-        percent -=10
-        if percent == -10:
-            template += f"\n{' '*y_axis}-----------------------"
-    y_axis = 5
-    x_values = len(categories.ledger[1:])
-    print(f"x_values: {x_values}")
-    return template
-
+        space_y = f"{' '*(y_axis - len(str(percent)+'|'))}"
+        bar_chart += f"\n{space_y}{percent}|"
+        for i in percent_spend_per_cate:
+            if i >= percent:
+                bar_chart += ' o '
+            else:
+                bar_chart += '   '
+        if percent == 0:
+            bar_chart += f"\n{' '*(y_axis)}{'-'*(3*len(categories)+1)}\n{' '*5}"
+            for i in range(max_len):
+                for letter in category_names:
+                    if i < len(letter):
+                        bar_chart += f"{letter[i]}  "
+                    else:
+                        bar_chart += ' '*3 #times 3 because there are 3 spaces 
+                if i != max_len - 1:
+                    bar_chart += f"\n{' '*5}"
+        percent -= 10
+    return bar_chart
 
 food = Category('Food')
-food.deposit(1000, 'deposit')
+food.deposit(900, 'deposit')
+food.withdraw(45.67, 'milk, cereal, eggs, bacon, bread')
+clothing = Category('Clothing')
 food.withdraw(10.15, 'groceries')
 food.withdraw(15.89, 'restaurant and more food for dessert')
-clothing = Category('Clothing')
 food.transfer(50, clothing)
-#print(food)
-print(create_spend_chart(food))
+clothing.withdraw(13.23, "socks")
+car = Category("Car")
+car.deposit(500, 'deposit')
+car.withdraw(236.45, 'new windshield')
+clothing.withdraw(19.78, 'pants')
+
+#print(f"{car}\n{food}\n{clothing}")
+print(create_spend_chart([food, car, clothing]))
--- build_budget_app_CP/smarter_main.py
+++ build_budget_app_CP/smarter_main.py
@@ -1,5 +1,4 @@
-#smarter_main.py is a better code made with using online resource while main.py 
-#is my first try without relying on the internet
+#smarter_main.py is a better code made with using online resource while main.py or an alternate version.
 class Category:
     total_balance = 0
     max_chars = 30
@@ -21,7 +20,7 @@ class Category:
         return True
     
     def get_balance(self):
-        return sum(i["amount"] for i in self.ledger)
+        return round(sum(i["amount"] for i in self.ledger), 2)
     
     def transfer(self, amount, new_category):
         if not self.check_funds(amount):
@@ -58,13 +57,51 @@ class Category:
 
 
 def create_spend_chart(categories):
-    pass
-
+    bar_chart = "Percentage spent by category"
+    category_names = list(n.name for n in categories) #the names of the categories
+    max_len = max(len(l) for l in category_names)
+    spending_by_cate = {}
+    for i in categories:
+        spent = -(sum(n["amount"] for n in i.ledger if n['amount'] < 0)) #this sums up the withdrawals from one category at a time
+        spending_by_cate[i.name] = spent # this adds the category name and the spent value in the category 
+    spending_by_cate["Total"] = sum(spending_by_cate.values()) #total spending for all categories
+    percent_spend_per_cate = list(int((100*spending_by_cate[n]/spending_by_cate["Total"])/10)*10 for n in list(spending_by_cate)[:-1])
+    #This will fetch the spending per category and divide it by the total spending to have a percentage of total spending per category
+    percent = 100
+    y_axis = 4
+    while percent >= 0:
+        space_y = f"{' '*(y_axis - len(str(percent)+'|'))}"
+        bar_chart += f"\n{space_y}{percent}|"
+        for i in percent_spend_per_cate:
+            if i >= percent:
+                bar_chart += ' o '
+            else:
+                bar_chart += '   '
+        if percent == 0:
+            bar_chart += f"\n{' '*(y_axis)}{'-'*(3*len(categories)+1)}\n{' '*5}"
+            for i in range(max_len):
+                for letter in category_names:
+                    if i < len(letter):
+                        bar_chart += f"{letter[i]}  "
+                    else:
+                        bar_chart += ' '*3 #times 3 because there are 3 spaces 
+                if i != max_len - 1:
+                    bar_chart += f"\n{' '*5}"
+        percent -= 10
+    return bar_chart
 
 food = Category('Food')
-food.deposit(1000, 'deposit')
+food.deposit(900, 'deposit')
+food.withdraw(45.67, 'milk, cereal, eggs, bacon, bread')
+clothing = Category('Clothing')
 food.withdraw(10.15, 'groceries')
 food.withdraw(15.89, 'restaurant and more food for dessert')
-clothing = Category('Clothing')
 food.transfer(50, clothing)
-print(food)
+clothing.withdraw(13.23, "socks")
+car = Category("Car")
+car.deposit(500, 'deposit')
+car.withdraw(236.45, 'new windshield')
+clothing.withdraw(19.78, 'pants')
+
+#print(f"{car}\n{food}\n{clothing}")
+print(create_spend_chart([food, car, clothing]))
